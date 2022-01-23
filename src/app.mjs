95d6d20a8f1e8656e3b0f6c94008db7d01ed81c3
Noor Latif <noor.crystal@gmail.com>
2022-01-23 14:09:22 +0100

    eslint: run code fixup
    Introduced two linting commands:
    npm run lint
    npm run fix
    
    Defined in package.json

--- src/app.mjs
+++ src/app.mjs
@@ -1,14 +1,14 @@
-import express from 'express';
-import * as math from './math.mjs';
+import express from 'express'
+import * as math from './math.mjs'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 app.get('/', function (req, res) {
-    res.send('Hello World!')
-});
+  res.send('Hello World!')
+})
 
 app.listen(port, function () {
-    console.log(`Example app listening on http://localhost:${port}/`)
-    console.log("Example calculation using custom library: 4 * 4 = \n" + math.area(4))
-});
+  console.log(`Example app listening on http://localhost:${port}/`)
+  console.log('Example calculation using custom library: 4 * 4 = \n' + math.area(4))
+})
